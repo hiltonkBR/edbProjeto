1fb78de94a965aa86398afbb6c095d907a1a6f8d
hiltonkBR <hilton.kbr@gmail.com>
2019-10-08 23:36:00 -0300

    Criação da classe Compressor e dos primeiros metodos para seu funcionamento
    Co-Authored-By: Raul Santos <raulmss@users.noreply.github.com>

---
+++ src/br/com/edb2/Compressor.java
@@ -0,0 +1,47 @@
+package br.com.edb2;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Compressor {
+    public static String iniciaCompressor(String arquivo, String arquivoFinal, String dicionario) throws IOException {
+        File arquivoOriginal = new File(arquivo);
+
+        if (arquivoOriginal.exists() && arquivoOriginal.isFile()) {
+            //Caso exista o arquivo
+            Path caminho = Paths.get(arquivo);
+            String stringDOarquivo = Files.readString(caminho);
+            //System.out.print(stringDOarquivo);
+            return stringDOarquivo;
+        } else {
+                //Caso não exista
+            return "Erro: arquivo informado não existe!\n";
+        }
+    }
+
+    public static Map<String, Integer> contaCaracteres(String texto) {
+        Map<String, Integer> map = new TreeMap<>();
+        for(char c: texto.toCharArray()) {
+            Integer i = map.get(String.valueOf(c));
+            if (i != null) {
+                map.put(String.valueOf(c), i+1);
+            }else {
+                map.put(String.valueOf(c), 1);
+            }
+        }
+        return map;
+    }
+
+    public static void imprimirCaracteres(Map<String,Integer> map) {
+        for(Map.Entry<String, Integer> m: map.entrySet()) {
+            System.out.println(m.getKey() + " : " + m.getValue());
+        }
+    }
+}
+
+
--- src/br/com/edb2/Main.java
+++ src/br/com/edb2/Main.java
@@ -1,8 +1,10 @@
 package br.com.edb2;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        Compressor.imprimirCaracteres(Compressor.contaCaracteres(Compressor.iniciaCompressor("C:\\testes\\teste1.txt","","")));
     }
 }
