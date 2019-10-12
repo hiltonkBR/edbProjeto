7acb74ab2e91f21492cf6c7d4b7bd06eb4c2862e
Raul Santos <raul.soutos96@gmail.com>
2019-10-12 17:54:44 -0300

    caracteres \n e EOF

--- src/br/com/edb2/Compressor.java
+++ src/br/com/edb2/Compressor.java
@@ -1,9 +1,6 @@
 package br.com.edb2;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,32 +8,44 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Compressor {
-    public static String iniciaCompressor(String arquivo, String arquivoFinal, String dicionario) throws IOException {
-        File arquivoOriginal = new File(arquivo);
+    public static Node iniciaCompressor(String arquivo, String arquivoFinal, String dicionario) throws IOException {
+        FileReader arquivoOriginal = null;
+        try{
+            arquivoOriginal = new FileReader(arquivo);
 
-        if (arquivoOriginal.exists() && arquivoOriginal.isFile()) {
-            //Caso exista o arquivo
-            Path caminho = Paths.get(arquivo);
-            String stringDOarquivo = Files.readString(caminho);
-            //System.out.print(stringDOarquivo);
-            return stringDOarquivo;
-        } else {
-            //Caso não exista
-            return "Erro: arquivo informado não existe!\n";
+                BufferedReader stringDOarquivo = new BufferedReader(arquivoOriginal);
+                return contaCaracteres(stringDOarquivo);
+
+    }catch (IOException e){
+        return null;
         }
     }
 
-    public static Map<Character, Integer> contaCaracteres(String texto) {
+    public static Node contaCaracteres(BufferedReader leitor) throws IOException {
         Map<Character, Integer> treemap = new TreeMap<>();
-        for (char c : texto.toCharArray()) {
-            Integer i = treemap.get(c);
-            if (i == null) {
-                treemap.put(c, 1);
-            } else {
-                treemap.put(c, i + 1);
+        String texto = leitor.readLine();
+        while(texto != null){
+            for (char c : texto.toCharArray()) {
+                Integer i = treemap.get(c);
+                if (i == null) {
+                    treemap.put(c, 1);
+                } else {
+                    treemap.put(c, i + 1);
+                }
+            }
+            texto = leitor.readLine();
+            if(texto != null){
+                Integer i = treemap.get((char)260); // \n
+                if (i == null) {
+                    treemap.put((char)260, 1);
+                } else {
+                    treemap.put((char)260, i + 1);
+                }
             }
         }
-        return treemap;
+        treemap.put((char)261, 1); // EOF
+        System.out.println(treemap);
+        return tree(treemap);
     }
 
     public static void imprimirCaracteres(Map<Character, Integer> map) {
--- src/br/com/edb2/Main.java
+++ src/br/com/edb2/Main.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-       Node no = Compressor.tree(Compressor.contaCaracteres(Compressor.iniciaCompressor("/home/raul/Área de Trabalho/Proj EDB2/testes/teste1.txt","","")));
+       Node no = Compressor.iniciaCompressor("/home/raul/Área de Trabalho/Proj EDB2/testes/teste1.txt","","");
     }
 }
