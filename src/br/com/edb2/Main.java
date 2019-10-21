0d2047f81043f86db56b83791cf9330f33ce3198
hiltonkBR <hilton.kbr@gmail.com>
2019-10-21 15:26:28 -0300

    Submissão final

--- src/br/com/edb2/Main.java
+++ src/br/com/edb2/Main.java
@@ -6,7 +6,12 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         //Compressor co = new Compressor("C:\\testes\\teste3.txt","C:\\testes\\teste3.edz","C:\\testes\\teste3.edt");
-        Extractor ex = new Extractor("C:\\testes\\teste3.edz","C:\\testes\\teste3.edt","C:\\testes\\teste3Restaurado.txt");
+        //Extractor ex = new Extractor("C:\\testes\\teste3.edz","C:\\testes\\teste3.edt","C:\\testes\\teste3Restaurado.txt");
 
+        if( args[0].equals(new String("compress"))){
+            Compressor co = new Compressor(args[1],args[2], args[3]);
+        }else if(args[0].equals(new String("extract"))){
+            Extractor ex = new Extractor(args[1],args[2], args[3]);
+        }
     }
 }
